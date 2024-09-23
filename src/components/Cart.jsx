4fab2ff182192a22fa9498659e5894a2d5c69766
Josh <119835493+josh76543210@users.noreply.github.com>
2024-09-22 22:13:12 -0700

    Add empty cart icon;

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,6 @@
 import CartItem from "./CartItem";
 import IconCarbonNeutral from "../assets/images/IconCarbonNeutral.svg";
+import EmptyCartIcon from "../assets/images/EmptyCart.svg";
 
 import styles from "./Cart.module.css";
 
@@ -9,39 +10,47 @@ function Cart() {
   const { dispatch, orderConfirmed, items } = useDessertOrderData();
 
   console.log(orderConfirmed);
+  const totalItems = items.filter((item) => item.quantity != 0).length;
 
   return (
     <div className={styles.cartContainer}>
-      <h2 className={styles.title}>
-        Your Cart ({items.filter((item) => item.quantity != 0).length})
-      </h2>
-      {items
-        .filter((item) => item.quantity != 0)
-        .map((item) => (
-          <CartItem item={item} key={item.name} />
-        ))}
-      <div className={styles.totalPrice}>
-        <p>Order Total</p>
-        <p>
-          $
+      <h2 className={styles.title}>Your Cart ({totalItems})</h2>
+      {totalItems ? (
+        <>
           {items
-            .map((items) => items.price * items.quantity)
-            .reduce((a, b) => a + b, 0)
-            .toFixed(2)}
-        </p>
-      </div>
-      <div className={styles.carbonNeutralBanner}>
-        <IconCarbonNeutral />
-        <p>
-          This is a <strong>carbon-neutral</strong> delivery
-        </p>
-      </div>
-      <button
-        onClick={() => dispatch({ type: "confirmOrder" })}
-        className={styles.confirmBtn}
-      >
-        Confirm Order
-      </button>
+            .filter((item) => item.quantity != 0)
+            .map((item) => (
+              <CartItem item={item} key={item.name} />
+            ))}
+          <div className={styles.totalPrice}>
+            <p>Order Total</p>
+            <p>
+              $
+              {items
+                .map((items) => items.price * items.quantity)
+                .reduce((a, b) => a + b, 0)
+                .toFixed(2)}
+            </p>
+          </div>
+          <div className={styles.carbonNeutralBanner}>
+            <IconCarbonNeutral />
+            <p>
+              This is a <strong>carbon-neutral</strong> delivery
+            </p>
+          </div>
+          <button
+            onClick={() => dispatch({ type: "confirmOrder" })}
+            className={styles.confirmBtn}
+          >
+            Confirm Order
+          </button>
+        </>
+      ) : (
+        <div className={styles.emptyCartContainer}>
+          <EmptyCartIcon />
+          <p>Your added items will appear here</p>
+        </div>
+      )}
     </div>
   );
 }
